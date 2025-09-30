0dbd1ba9e790c2de7516eca3f634fd4739fcee3a
x-Adrian-x <adrianesteban98@hotmail.com>
2025-09-29 21:06:47 -0500

    Agrega otro input

---
+++ main.js
@@ -0,0 +1,6 @@
+const formT = document.getElementsById("task-form");
+const nameT = document.getElementById("task-title");
+const descT = document.getElementById("task-description");
+const ownerT = document.getElementById("task-owner");
+const btnT = document.getElementById("task-create");
+
